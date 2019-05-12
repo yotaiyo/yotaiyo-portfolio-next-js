a8752f18a11eae2cd1a3bb7cb7ebb390c73e1e92
yotaiyo <yota071218@gmail.com>
2019-05-12 15:09:01 +0900

    use styled-component

--- components/Header.js
+++ components/Header.js
@@ -2,17 +2,24 @@ import Link from 'next/link'
 import styled from 'styled-components'
 import homeIcon from '../images/home.png'
 
-const HomeIcon = styled.img``
+const Icon = styled.img`
+  width: 30px;
+  height: 30px;
+`
+
+const Body = styled.div``
 
 const Header = () => (
     <div>
-        <Link href="/">
-          <a>Home</a>
-        </Link>
-        <Link href="/about">
-          <a>About</a>
-        </Link>
-        <HomeIcon src={homeIcon} />
+      <Link href="/">
+        <Icon src={homeIcon} alt='homeIcon' />
+      </Link>
+      <Link href="/">
+        <Body>Home</Body>
+      </Link>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
     </div>
 )
 
