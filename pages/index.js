1a5fa743e7c42de660c7869ad6371784175eedf0
yotaiyo <yota071218@gmail.com>
2019-05-14 00:39:05 +0900

    use media query to Home

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,21 @@ import MyImage from '../images/my-icon.jpg'
 const Wrapper = styled.div`
     padding-top: 200px;
     padding-bottom: 200px;
+    @media (max-width: 768px) {
+        padding-top: 150px;
+        padding-bottom: 150px;
+    }
 `
 
 const MyIcon = styled.img`
     height: 250px;
     width: 250px;
     border-radius: 250px;
+    @media (max-width: 768px) {
+        height: 150px;
+        width: 150px;
+        border-radius: 150px;
+    }
 `
 
 const MyIconWrapper = styled.div`
@@ -24,6 +33,9 @@ const Title = styled.div`
     font-size: 70px;
     margin-top: 30px;
     color: white;
+    @media (max-width: 768px) {
+        font-size: 35px;
+    }
 `
 
 export default () => (
