644cc557b3230c182eb1f4da6ebdf46fe079d7c0
yotaiyo <yota071218@gmail.com>
2019-05-13 21:30:07 +0900

    add link to repos

--- pages/repos.js
+++ pages/repos.js
@@ -1,5 +1,6 @@
 import Layout from '../components/MyLayout.js'
 import styled from 'styled-components'
+import LinkImage from '../images/link.png'
 
 const Wrapper = styled.div`
     display: flex;
@@ -24,10 +25,20 @@ const ReposCardWrapper = styled.div`
     border-radius: 30px;
     height: 150px;
 `
+const ReposTitleWrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+`
 
 const ReposTitle = styled.div`
     font-size: 25px;
-    text-align: center;
+`
+
+const LinkIcon = styled.img`
+    width: 15px;
+    height: 15px;
+    margin-top: 12px;
 `
 
 const ReposBody = styled.div`
@@ -41,9 +52,14 @@ const ReposKeywords = styled.div`
     margin-top: 20px;
 `
 
-const ReposCard = ({ title, body, keywords }) => (
+const ReposCard = ({ title, body, keywords, url }) => (
     <ReposCardWrapper>
-        <ReposTitle>{title}</ReposTitle>
+        <ReposTitleWrapper>
+            <ReposTitle>{title}</ReposTitle>
+            <a href={url}>
+                <LinkIcon src={LinkImage} alt='link-image' style={{ marginLeft: 8 }} />
+            </a>
+        </ReposTitleWrapper>
         <ReposBody>{body}</ReposBody>
         <ReposKeywords>{'keywords: ' + keywords}</ReposKeywords>
     </ReposCardWrapper>
@@ -69,21 +85,25 @@ export default () => (
                         title='yotaiyo-portfolio-next-js' 
                         body='このポートフォリオです。' 
                         keywords='JavaScript, Next.js, Firebase'
+                        url='https://github.com/yotaiyo/yotaiyo-portfolio-next-js'
                     />
                     <ReposCard 
                         title='yotaiyo-portfolio-react' 
                         body='ポートフォリオ第1弾。' 
                         keywords='JavaScript, React, Github.io'
+                        url='https://github.com/yotaiyo/yotaiyo-portfolio-react'
                     />
                     <ReposCard 
                         title='music-auto-tagging' 
                         body='楽曲波形から、そのジャンルや楽器、ムードといったタグを予測するレポジトリ。MagnaTagATuneと呼ばれる大規模データセットを用いる。' 
                         keywords='Python3, Keras, TensorFlow, Scikit-learn, GPU'
+                        url='https://github.com/yotaiyo/music-auto-tagging'
                     />
                     <ReposCard 
                         title='music-genre-classification' 
                         body='楽曲波形から、そのジャンルを予測するレポジトリ。GTZANと呼ばれるデータセットを用いる。' 
                         keywords='Python3, Keras, TensorFlow, Scikit-learn, GPU'
+                        url='https://github.com/yotaiyo/music-genre-classification'
                     />
                 </ReposLeft>
                 <ReposRight>
@@ -91,16 +111,19 @@ export default () => (
                         title='weather-app-react' 
                         body='お天気APIを叩いて、向こう3日間の情報を表示する。' 
                         keywords='JavaScript, React, Github.io'
+                        url='https://github.com/yotaiyo/weather-app-react'
                     />
                     <ReposCard 
                         title='jazz-genre-classification-svm' 
                         body='ジャズ好きが高じて作成したレポジトリ。音源波形からBopなどのサブジャンルを予測する。' 
                         keywords='Python3, Scikit-learn'
+                        url='https://github.com/yotaiyo/jazz-genre-classification-svm'
                     />
                     <ReposCard 
                         title='jazz-genre-classification-cnn' 
                         body='ジャズ好きが高じて作成したレポジトリ2。音源波形からBopなどのサブジャンルを予測する。' 
                         keywords='Python3, TensorFlow, Scikit-learn'
+                        url='https://github.com/yotaiyo/jazz-genre-classification-cnn'
                     />
                 </ReposRight>
             </ReposWrapper>
