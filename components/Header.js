6f7d95c1bfdf5baa0d22bba25bec7a71402a3e4c
yotaiyo <yota071218@gmail.com>
2019-09-29 18:56:28 +0900

    以前のファイルを削除

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,4 @@
-import React, { Component } from 'react'
-import Link from 'next/link'
+import React from 'react'
 import styled from 'styled-components'
 import { Layout } from '../public/Layout'
 
