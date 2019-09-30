f565f749db8897e5c8811ed499c657453658139d
yotaiyo <yota071218@gmail.com>
2019-09-30 12:37:49 +0900

    自分のレポジトリの情報を取得

--- pages/works.js
+++ pages/works.js
@@ -1,15 +1,30 @@
 import React from 'react'
 import Layout from '../components/MyLayout'
 import styled from 'styled-components'
+import Octokit from '@octokit/rest'
 
 const Wrapper = styled.div``
 
+export async function getRepos(userName) {
+  const octokit = new Octokit()
+
+  const res = await octokit.search.repos({ q: `user:${userName}` })
+  const repos = res.data.items
+
+  return repos
+}
+
 export default class Works extends React.Component {
   static async getInitialProps() {
-    return { a: 'a' }
+    const repos = await getRepos('yotaiyo')
+    return {
+      repos
+    }
   }
 
   render() {
+    const { repos } = this.props
+    console.log(repos)
     return (
       <Layout>
         <Wrapper>test</Wrapper>
