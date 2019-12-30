2851d9e1224b90541213a9a843268ef1599320d0
yotaiyo <yota071218@gmail.com>
2019-12-30 23:58:49 +0900

    rootReducer.tsを作成

--- src/app/makeStore.ts
+++ src/app/makeStore.ts
@@ -1,12 +1,11 @@
-import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import { github, GithubState } from '../../reducers/github';
+import { GithubState } from '../../reducers/github';
+import { rootReducer } from './rootReducer';
 
 export type InitialState = {
   github: GithubState;
 };
-
-const rootReducer = combineReducers({ github });
 
 export const makeStore = (initialState: InitialState) => {
   return createStore(rootReducer, initialState, applyMiddleware(thunk));
---
+++ src/app/rootReducer.ts
@@ -0,0 +1,4 @@
+import { combineReducers } from 'redux';
+import { github } from '../../reducers/github';
+
+export const rootReducer = combineReducers({ github });
