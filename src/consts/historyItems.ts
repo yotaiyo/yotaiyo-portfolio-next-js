feb6dc8e86e4e1fe3f7823474ade62e73dcac55c
yotaiyo <yota071218@gmail.com>
2019-12-31 02:08:37 +0900

    consts配下にhistoryItemsを切り出す

---
+++ src/consts/historyItems.ts
@@ -0,0 +1,34 @@
+export const historyItems = [
+  {
+    period: '2020.01 ~ ',
+    title: 'エムスリー 長期インターン',
+    body: 'Comming Soon',
+    tags: []
+  },
+  {
+    period: '2019.09 ~ 12',
+    title: 'eureka 長期インターン',
+    body: 'フロントエンドエンジニアとして、Pairs Web版の開発に参加。',
+    tags: ['typescript', 'react', 'redux']
+  },
+  {
+    period: '2019.09',
+    title: 'DMM.com 短期インターン',
+    body:
+      'クエスト型インターン、DMM GUILDに参加。DMMが実際に抱えている技術的な課題に挑戦。',
+    tags: ['vue', 'react', 'redux', 'vanillajs']
+  },
+  {
+    period: '2019.01 ~ 08',
+    title: 'FIREBUG 長期インターン',
+    body: 'toB向けマッチングアプリ開発に参加。',
+    tags: ['typescript', 'reactnative', 'graphql']
+  },
+  {
+    period: '2018.12 ~ 01',
+    title: 'Apollo 短期インターン',
+    body:
+      'webエンジニアリングに関する基礎を学ぶ。その後、業務委託のアプリ開発案件に参加。',
+    tags: ['html/css', 'typescript', 'jquery', 'react']
+  }
+];
