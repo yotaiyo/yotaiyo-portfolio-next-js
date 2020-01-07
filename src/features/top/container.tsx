326c659ddd21ac1f5a433a2e1f0273cda4059f46
yotaiyo <yota071218@gmail.com>
2020-01-07 17:46:04 +0900

    topのコンポーネントをtemplates配下に移動

--- src/features/top/container.tsx
+++ src/features/top/container.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import MyLayout from 'src/common/components/MyLayout';
 import styled from 'styled-components';
-import { Profile } from './components/Profile';
-import { History } from './components/History';
-import { Links } from './components/Links';
+import { Profile } from './components/templates/Profile';
+import { History } from './components/templates/History';
+import { Links } from './components/templates/Links';
 
 const Wrapper = styled.div`
   margin-bottom: 100px;
