02eb5575e12a2ad96135083421d601a73fa1c0ee
yotaiyo <yota071218@gmail.com>
2020-03-12 20:44:10 +0900

    containerをpagesフォルダ配下に移動

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,24 @@
-import TopContainer from 'src/features/top/container';
+import React from 'react';
+import MyLayout from 'src/common/layout';
+import styled from 'styled-components';
+import { Profile } from 'src/features/top/components/templates/Profile';
+import { History } from 'src/features/top/components/templates/History';
+import { Links } from 'src/features/top/components/templates/Links';
+
+const Wrapper = styled.div`
+  margin-bottom: 100px;
+`;
+
+const TopContainer = () => {
+  return (
+    <MyLayout pathname={'/'}>
+      <Wrapper>
+        <Profile />
+        <History />
+        <Links />
+      </Wrapper>
+    </MyLayout>
+  );
+};
 
 export default TopContainer;
