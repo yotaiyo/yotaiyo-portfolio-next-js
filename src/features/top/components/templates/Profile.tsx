7cde9a0c5aa70e34b1f5ed6a2f39d5dfa3523e97
yotaiyo <yota071218@gmail.com>
2020-03-21 09:38:47 +0900

    カードに少し丸みをつける

--- src/features/top/components/templates/Profile.tsx
+++ src/features/top/components/templates/Profile.tsx
@@ -42,11 +42,14 @@ const CardWrapper = styled.div`
   width: 500px;
   box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2),
     0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0, 0, 0, 0.12);
+  border-radius: 2px;
 `;
 
 const MyHeader = styled.img`
   width: 500px;
   height: 250px;
+  border-top-left-radius: 2px;
+  border-top-right-radius: 2px;
 `;
 
 type CardBottomWrapperType = {
